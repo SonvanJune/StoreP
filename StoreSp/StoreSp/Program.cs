2e3bc69ce336e1db2624d96ec9b7fabb6f89ac90
SonVanJune <sonthu3333@gmail.com>
2024-08-18 11:00:39 +0700

    Fix

--- StoreSp/StoreSp/Program.cs
+++ StoreSp/StoreSp/Program.cs
@@ -10,7 +10,7 @@ using StoreSp.Stores;
 var builder = WebApplication.CreateBuilder(args);
 builder.RunConfig();
 
-Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", @"Ids\storep-d7a1c-firebase-adminsdk-2a02r-bfa325ae0a.json");
+Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", @"D:\speed-5046d-firebase-adminsdk-i9v5v-a8e81bb926.json");
 FirestoreDb db = FirestoreDb.Create(builder.Configuration.GetConnectionString("ProjectId"));
 FirestoreService.Run(db, builder.Configuration.GetConnectionString("ProjectId")!);
 
